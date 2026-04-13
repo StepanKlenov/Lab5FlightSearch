4b64bf1dc9e941377180108734480bdd82bf816f
stepanklenov <stodx@icloud.com>
2026-04-13 21:13:17 +0400

    chore: setup project structure

---
+++ app/src/main/java/com/example/lab5flightsearch/data/local/entity/Airport.kt
@@ -0,0 +1,12 @@
+package com.example.lab5flightsearch.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "airport")
+data class Airport(
+    @PrimaryKey val id: Int,
+    val iata_code: String,
+    val name: String,
+    val passengers: Int
+)
